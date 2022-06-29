7e51694f2451dd42a23e19debd2114ea1464a3d1
Ambarish Nagarajan <41073593+Ambu1988@users.noreply.github.com>
2022-06-29 22:37:02 +0800

    Preparation Chapter JS Exercises

---
+++ Preparations/preparations_exercises/passByRef.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    homePlanet : 'Earth',
+    color : 'red'
+  };
+  let tryReassignment = obj => {
+    obj = {
+      identified : false, 
+      'transport type' : 'flying'
+    }
+    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+   
+  };
+  tryReassignment(spaceship);
