a90257c9243e353da4815e258f6c58aaf0af936d
Ambarish Nagarajan <41073593+Ambu1988@users.noreply.github.com>
2022-06-29 23:32:16 +0800

    more exercises on basics

---
+++ Preparations/preparations_exercises/basics.js
@@ -0,0 +1,14 @@
+console.log('Ambarish' + ' ' + 'Nagarajan');
+
+//Create an array named names that contains a list of pet names. For instance:
+const names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+console.log(names);
+//create an object
+const pets = {
+asta: 'dog',
+butterscotch: 'cat',
+pudding: 'cat',
+neptune: 'fish',
+darwin: 'lizard',
+};
+console.log(pets);
